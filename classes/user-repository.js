ce90f637cd9708d22e86a5ff18f1d1fdf95460f3
Chriscastanuela <chriscastanuela@ymail.com>
2020-08-26 13:23:55 -0600

    Add user-repository

---
+++ classes/user-repository.js
@@ -0,0 +1,25 @@
+class UserRepository {
+    constructor(data) {
+        this.userObjects = data;
+    }
+    returnData(userId) {
+        return this.userObjects.find(index => {
+            return index.id == userId;
+        });
+    };
+    avgStepGoalOfAllUsers() {
+        var sum = this.userObjects.reduce((avg, object) => {
+
+            avg += object.dailyStepGoal;
+            console.log(avg);
+            return avg;
+
+        }, 0)
+
+        let avg = sum / this.userObjects.length;
+
+        return avg;
+    }
+};
+
+module.exports = UserRepository;
