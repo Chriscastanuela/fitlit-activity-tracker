d04047d6c73866471799c24f42c4163e56b1a96d
Estelle Staffieri <estellelareinne@gmail.com>
2020-08-26 17:18:40 -0600

    Add global queryselectors for initial User Info card section.

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,32 @@
+//global variables and queryselectors
+var userGreeting = document.queryselector(".Hi-User");
+var userStepGoalDis = document.queryselector(".Step-Goals");
+var userProfileImage = document.queryselector(".Main-User-Icon");
+var friendOne = document.queryselector(".Friend-1");
+var friendTwo = document.queryselector(".Friend-2");
+var friendThree = document.queryselector(".Friend-3");
+var friendFour = document.queryselector(".Friend-4");
+var friendOneIcon = document.queryselector(".icon-1");
+var friendTwoIcon = document.queryselector(".icon-2");
+var friendThreeIcon = document.queryselector(".icon-3");
+var friendFourIcon = document.queryselector(".icon-4");
+var friendOneIcon = document.queryselector(".Friend-Name-1");
+var friendTwoIcon = document.queryselector(".Friend-Name-2");
+var friendThreeIcon = document.queryselector(".Friend-Name-3");
+var friendFourIcon = document.queryselector(".Friend-Name-4");
+var defaultUser = {
+  "id": 7,
+  "name": "Breanne Fay",
+  "address": "834 Retta Knoll, Stantonland MA 71627-4121",
+  "email": "Dashawn28@gmail.com",
+  "strideLength": 2.9,
+  "dailyStepGoal": 8000,
+  "friends": [
+    12,
+    27,
+    22,
+    30
+  ]
+};
+
 console.log("Hello World");
