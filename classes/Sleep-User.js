d294286c0334ad23c2a4ad7cbf2838922192c9c7
Chriscastanuela <chriscastanuela@ymail.com>
2020-08-27 16:43:09 -0600

    Start Sleep Method

--- classes/Sleep-User.js
+++ classes/Sleep-User.js
@@ -1,5 +1,11 @@
 class SleepUser {
-    constructor() {
+    constructor(sleepData) {
+        this.sleepData = sleepData;
+    }
+    avgSleepHours(userID) {
+        let userSleepObjects = sleepData.filter(sleepObject => {
+            return sleepObject.userID == userID;
+        });
         //
     }
 }
