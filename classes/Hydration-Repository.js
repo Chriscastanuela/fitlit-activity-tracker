0b4435c38418754394c46ba6fbb3feb6bd7a071c
Chriscastanuela <chriscastanuela@ymail.com>
2020-08-28 13:37:30 -0600

    Update Hydration Methods

--- classes/Hydration-Repository.js
+++ classes/Hydration-Repository.js
@@ -5,7 +5,7 @@ class HydrationRepository {
     averageFlOzAllTime(userId) {
         let userHydrationDataList = this.hydrationData.filter(hydrationObj => {
             return hydrationObj.userID == userId;
-        })
+        });
         let sum = userHydrationDataList.reduce((avg, object) => {
             avg += object.numOunces;
             return avg;
@@ -13,18 +13,8 @@ class HydrationRepository {
         let average = sum / userHydrationDataList.length;
         return average;
     }
-    flOzOnAGivenDay(userID, date) {
-        let dateArray = this.hydrationData.filter(dataObject => {
-            return dataObject.date == date;
-        })
-        let ounces = dateArray.find(dataObject => {
-            return dataObject.userID == userID;
-        })
-        return ounces;
-    }
-    lastWeekOfWaterData(userID) {
-        //
-    }
 }
 
-module.exports = HydrationRepository;
+if (typeof module !== "undefined") {
+    module.exports = HydrationRepository;
+}
--- classes/Hydration.js
+++ classes/Hydration.js
@@ -1,9 +1,24 @@
 class Hydration {
-    constructor(object) {
-        this.userID = object.userID;
-        this.date = object.date;
-        this.numOunces = object.numOunces;
+    constructor(userHydrationData) {
+        this.userHydrationData = userHydrationData;
     }
+    flOzOnAGivenDay(date) {
+        let dateData = this.userHydrationData.find(dataObject => {
+            return dataObject.date == date;
+        });
+        return dateData.numOunces;
+    }
+    lastWeekOfWaterData() {
+        let sortedData = this.userHydrationData.sort((a, b) =>a - b);
+        let lastSeven = sortedData.slice(-7);
+        //
+        console.log(lastSeven);
+        //
+        let lastSevenOzOnly = lastSeven.map(object => {
+            return object.numOunces;
+        })
+        return lastSevenOzOnly;
+    };
 };
 
 module.exports = Hydration;
