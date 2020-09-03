540c0750b6dca8f486ad36356765da50ad425bc2
Chriscastanuela <chriscastanuela@ymail.com>
2020-09-02 18:11:31 -0600

    Refactor chart

--- src/scripts.js
+++ src/scripts.js
@@ -35,48 +35,6 @@ let activity = new Activity(activityRepository.allDataOfOneUser(1));
 let hydrationRepository = new HydrationRepository(hydrationData);
 let hydration = new Hydration(hydrationRepository.returnData(20));
 
-//---------------------------Chart Scripts
-/*
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-*/
-
-
 //---------------------------Function Declarations
 function displayDate(date) {
     todayDate.innerHTML = `${date}`
@@ -118,7 +76,7 @@ function displayTodayHydration(date) {
 
 function displayWeeklyyHydration() {
     console.log(hydration.lastWeekOfWaterData());
-    weekHydration.innerHTML = `Weekly hydration info: ${hydration.lastWeekOfWaterData()}`
+    weekHydration.innerHTML = `Weekly hydration info: ${(hydration.lastWeekOfWaterData()[0].date)}: ${hydration.lastWeekOfWaterData()[0].numOunces} ounces`;
 }
 
 function displayTodaySteps(date, user) {
