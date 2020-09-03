d72a24c46b48b8f21cbb5befe253198966cc9a60
Chriscastanuela <chriscastanuela@ymail.com>
2020-09-02 19:15:08 -0600

    Add Sleep and Hydration charts

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,6 @@
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
 var userGreeting = document.querySelector(".Hi-User");
 var userStepGoalDis = document.querySelector(".Step-Goals");
 var userProfileImage = document.querySelector(".Main-User-Icon");
@@ -58,7 +61,43 @@ function displayUserLastDayOfSleep(date, id){
 }
 
 function displayUserLastWeekOfSleep(startDate, endDate, userId) {
-    weekSleep.innerHTML = `<p>Sleep insights for the past week: ${userSleep.findUserWeeklyData(startDate, endDate, userId)}</p>`
+    weekSleep.innerHTML = `<p>Sleep insights for the past week: <br><br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[0].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[0].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[1].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[1].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[2].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[2].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[3].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[3].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[4].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[4].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[5].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[5].hoursSlept} hours slept<br>
+    ${userSleep.findUserWeeklyData(startDate, endDate, userId)[6].date}: ${userSleep.findUserWeeklyData(startDate, endDate, userId)[6].hoursSlept} hours slept<br>`
+    var ctx = document.getElementById('sleepChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ["16", "17", "18", "19", "20", "21", "22"],
+        datasets: [{
+            label: 'Hours of sleep',
+            data: [6.1,5.3,4.2,6.7,10.5,7.7,10.9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+});
 }
 
 function displayAllTimeSleepAverages(user) {
@@ -75,13 +114,60 @@ function displayTodayHydration(date) {
 }
 
 function displayWeeklyyHydration() {
-    console.log(hydration.lastWeekOfWaterData());
-    weekHydration.innerHTML = `Weekly hydration info: ${(hydration.lastWeekOfWaterData()[0].date)}: ${hydration.lastWeekOfWaterData()[0].numOunces} ounces`;
+    weekHydration.innerHTML = `Weekly hydration info: <br><br>
+    ${(hydration.lastWeekOfWaterData()[0].date)}: ${hydration.lastWeekOfWaterData()[0].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[1].date)}: ${hydration.lastWeekOfWaterData()[1].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[2].date)}: ${hydration.lastWeekOfWaterData()[2].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[3].date)}: ${hydration.lastWeekOfWaterData()[3].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[4].date)}: ${hydration.lastWeekOfWaterData()[4].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[5].date)}: ${hydration.lastWeekOfWaterData()[5].numOunces} ounces<br>
+    ${(hydration.lastWeekOfWaterData()[6].date)}: ${hydration.lastWeekOfWaterData()[6].numOunces} ounces`
+    var ctx = document.getElementById('hydrationChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ["16", "17", "18", "19", "20", "21", "22"],
+        datasets: [{
+            label: 'Ounces of Water',
+            data: [23,75,22,60,66,34,96],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    min: 0,
+                    max: 120,
+                    stepSize: 20,
+                }
+            }]
+        }
+    }
+});
 }
 
 function displayTodaySteps(date, user) {
     todaySteps.innerHTML = `Today you took ${activity.stepsTakenOnAGivenDate(date)} steps and walked ${activity.milesWalked(date, user)} miles.<br><br>`
-    avgSteps.innerHTML = `Everyone else's step data: <br>
+    avgSteps.innerHTML = `Everyone else's step data: <br><br>
     Average minutes active: ${activityRepository.allUsersMinutesActiveOnAGivenDate(date)}<br>
     Average stairs climbed: ${activityRepository.stairsClimbedOnAGivenDate(date)}<br><br>`
 }
@@ -92,7 +178,7 @@ function displayTodayMinutesActive(date) {
 
 function displayWeekSteps(startDate, endDate) {
     // ${activity.findSevenDayStepCount(startDate, endDate)}
-    weekSteps.innerHTML = `Step counts for the past week:`
+    weekSteps.innerHTML = `Step counts for the past week: ${activity.findSevenDayStepCount(startDate, endDate)}`
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'line',
@@ -101,7 +187,7 @@ function displayWeekSteps(startDate, endDate) {
         labels: ["2019/09/15", "2019/09/16", "2019/09/17", "2019/09/18", "2019/09/19", "2019/09/20", "2019/09/21"],
         datasets: [{
             label: 'Number of Steps',
-            data: [10028,11067,4901,9974,12083,14000,5711,8072],
+            data: [11067,4901,9974,12083,14000,5711,8072],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -139,15 +225,11 @@ function displayWeekSteps(startDate, endDate) {
 //------------------------------------------Onload
 window.onload = displayNewUser();
 window.onload = displayUserLastDayOfSleep("2019/09/22", currentUser.id);
-window.onload = displayUserLastWeekOfSleep("2019/09/15", "2019/09/22", currentUser.id)
+window.onload = displayUserLastWeekOfSleep("2019/09/16", "2019/09/22", currentUser.id)
 window.onload = displayAllTimeSleepAverages(currentUser.id);
 window.onload = displayTodayHydration("2019/09/22");
 window.onload = displayWeeklyyHydration();
 window.onload = displayTodaySteps("2019/09/22", currentUser);
 window.onload = displayTodayMinutesActive("2019/09/22");
 window.onload = displayWeekSteps("2019/09/15", "2019/09/21");
-window.onload = displayDate("09/03/2020");
-
-/*
-
-*/
+window.onload = displayDate(date);
